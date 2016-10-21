b54fd61f675595fdf24ba09782cfd23a1710bf95
Andrey Myznikov <andrey.myznikov@gmail.com>
2016-10-21 03:29:27 +0300

    Bugfixes and improvements

--- opencv-sagai-test-1/main.cc
+++ opencv-sagai-test-1/main.cc
@@ -20,6 +20,15 @@
 using namespace cv;
 using namespace std;
 
+
+enum algo {
+  algo_sub,
+  algo_div
+};
+
+
+
+
 #define DEFAULT_MKDIR_MODE (S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH)
 
 static bool create_path(const char * path, mode_t mode = DEFAULT_MKDIR_MODE)
@@ -117,12 +126,12 @@ static void calc_hist(const Mat & image, double bins[], size_t numbins, double m
 
 static int central_region_radius = 150;
 
-static void get_energy_metrics(const Mat & image, double * pwr, double * mean)
+static void get_energy_metrics(const Mat & image, double * mean, double * pwr)
 {
   const double rr = min(min(central_region_radius, image.rows), image.cols);
   const double x0 = image.cols / 2, y0 = image.rows / 2;
 
-  *pwr = 0, * mean = 0;
+  *pwr = 0, *mean = 0;
 
   size_t npix = 0;
 
@@ -130,15 +139,24 @@ static void get_energy_metrics(const Mat & image, double * pwr, double * mean)
     const float * row = reinterpret_cast<const float*>(image.ptr(y));
     for ( int x = 0; x < image.cols; x++ ) {
       if ( hypot(x - x0, y - y0) < rr ) {
-        * pwr += row[x] * row[x];
-        * mean += row[x];
+        *mean += row[x];
         ++npix;
       }
     }
   }
-
   *mean /= npix;
-  *pwr = sqrt(*pwr);
+
+
+  for ( int y = 0; y < image.rows; y++ ) {
+    const float * row = reinterpret_cast<const float*>(image.ptr(y));
+    for ( int x = 0; x < image.cols; x++ ) {
+      if ( hypot(x - x0, y - y0) < rr ) {
+        *pwr += (row[x] - *mean) * (row[x] - *mean);
+      }
+    }
+  }
+
+  *pwr = sqrt(*pwr / npix) ;
 }
 
 
@@ -158,13 +176,31 @@ static void threshold_pixels(Mat & image, double minval, double maxval)
   }
 }
 
-
-static int global_field_blur_size = 100;
-
-static bool load_image(Mat & image, const string & fname, double * pwr)
+static void normalize_pixels(Mat & image, double smin, double smax, double dmin, double dmax)
 {
-  Mat fld, tmp;
-  double mv;
+  for ( int y = 0; y < image.rows; y++ ) {
+    float * row = reinterpret_cast<float*>(image.ptr(y));
+    for ( int x = 0; x < image.cols; x++ ) {
+      if ( row[x] < smin ) {
+        row[x] = dmin;
+      }
+      else if ( row[x] >= smax ) {
+        row[x] = dmax;
+      }
+      else {
+        row[x] = (row[x] - smin) * (dmax - dmin) / (smax - smin);
+      }
+    }
+  }
+}
+
+
+static int flat_field_blur_size = 100;
+static int local_field_blur_size = 3;
+
+static bool load_image(Mat & image, const string & fname)
+{
+  Mat fld;
 
   if ( !(image = imread(fname, IMREAD_UNCHANGED)).data ) {
     fprintf(stderr, "imread(%s) fails\n", fname.c_str());
@@ -173,43 +209,55 @@ static bool load_image(Mat & image, const string & fname, double * pwr)
 
   image.convertTo(image, CV_32FC1);
 
-  //blur(image, image, Size(1, 1));
-  blur(image, fld, Size(global_field_blur_size, global_field_blur_size));
+  if ( local_field_blur_size > 0 ) {
+    blur(image, image, Size(local_field_blur_size, local_field_blur_size));
+  }
 
-  get_energy_metrics(image, pwr, &mv);
-  (image -= mv) /= fld;
-  get_energy_metrics(image, pwr, &mv);
-
-  printf("%s\n", fname.c_str());
+  if ( flat_field_blur_size > 0 ) {
+    blur(image, fld, Size(flat_field_blur_size, flat_field_blur_size));
+    image /= fld;
+  }
 
   return true;
 }
 
 
-void dump_hist(const Mat & image)
+void dump_hist(const char * fname, const Mat & image, double gmin, double gmax)
 {
-  const size_t numbins = 256;
+  const size_t numbins = 100;
   double bins[numbins] = { 0 };
   double p = 0, vlo, vhi;
   size_t lo = 0, hi = numbins - 1;
   double minval, maxval, range;
 
+  FILE * fp = NULL;
+
+
   printf("=====================\n");
 
   minMaxLoc(image, &minval, &maxval);
-
   range = maxval - minval;
-  //printf("minVal=%g\tmaxVal=%g\trange=%g\n", minval, maxval, range);
+  printf("%s global: minVal=%g\tmaxVal=%g\trange=%g\n", fname, minval, maxval, range);
 
+  minval = gmin;
+  maxval = gmax;
+  range = maxval - minval;
   calc_hist(image, bins, numbins, minval, maxval);
 
   for ( size_t i = 0; i < numbins; ++i ) {
     p += bins[i];
   }
 
+  if ( !(fp = fopen(fname, "w")) ) {
+    fprintf(stderr, "Fatal: can not open %s: %s\n", fname, strerror(errno));
+    fp = stdout;
+  }
+
+
+  fprintf(fp, "BIN\tCENTER\tPOW\n");
   for ( size_t i = 0; i < numbins; ++i ) {
     bins[i] *= 100 / p;
-    printf("%3zu\t%+12.3g\t%+12.3g\n", i, minval + i * range / numbins, bins[i]);
+    fprintf(fp, "%3zu\t%+12.3g\t%+12.3g\n", i, minval + i * range / numbins, bins[i]);
   }
 
   for( double s = 0; lo < numbins; ++lo ) {
@@ -230,6 +278,11 @@ void dump_hist(const Mat & image)
   vhi = minval + hi * range / numbins;
   printf("lo=%zu\thi=%zu\tvlo=%g\tvhi=%g\n", lo, hi, vlo, vhi);
 
+  if ( fp && fp != stdout ) {
+    fclose(fp);
+  }
+
+
 }
 
 int main(int argc, char *argv[])
@@ -241,12 +294,24 @@ int main(int argc, char *argv[])
   vector<string> input_files;
 
   Mat prev, current, diff;
-  double first_pwr, current_pwr;
+  double first_pwr = 0, current_pwr = 0;
+  double first_mean = 0, current_mean = 0;
 
   size_t i, j;
 
-  double gmin = -0.08, gmax = 0.08;
+  enum algo algo = algo_div;
+
+  double gmin_sub = -0.08, gmax_sub = 0.08;
+  double gmin_div = -0.1, gmax_div = 0.1;
+
+  double gmin, gmax, gamma = 1;
+  bool gmin_set = false, gmax_set = false;
+
+  double alpha = 0.1;
+
   bool dump_histogram = false;
+
+
 
   for( int i = 1; i < argc; ++i ) {
 
@@ -256,11 +321,14 @@ int main(int argc, char *argv[])
       printf("OPTIONS:\n");
       printf("  -o <output-directory-for-processed-images> [%s] \n", output_directory_name);
       printf("  -g  dump internal histogram\n");
-      printf("  gmin=<low-output-threshod> [%g] \n", gmin);
-      printf("  gmax=<high-output-threshod> [%g] \n", gmax);
-      printf("  b=<global-field-blur-size> [%d] \n", global_field_blur_size);
+      printf("  gmin=<low-output-threshod> [sub=%g div=%g] \n", gmin_sub, gmin_div);
+      printf("  gmax=<high-output-threshod> [sub=%g div=%g] \n", gmax_sub, gmax_div);
+      printf("  gamma=<gamma-correction-of-output-frames> [%g] \n", gamma);
+      printf("  alpha=<contrast-parameter> [%g]\n", alpha);
+      printf("  b=<flat-field-blur-size> [%d] \n", flat_field_blur_size);
+      printf("  f=<local-field-blur-size> [%d] \n", local_field_blur_size);
       printf("  cr=<central_region_radius> [%d] \n", central_region_radius);
-
+      printf("  a=<sub|div> select 'frame difference' algorithm\n");
 
       return 0;
     }
@@ -278,20 +346,52 @@ int main(int argc, char *argv[])
     else if ( strcmp(argv[i], "-g") == 0 ) {
       dump_histogram = true;
     }
+    else if ( strncmp(argv[i], "a=", 2) == 0 ) {
+      if ( strcmp(argv[i] + 2, "sub") == 0 ) {
+        algo = algo_sub;
+      }
+      else if ( strcmp(argv[i] + 2, "div") == 0 ) {
+        algo = algo_div;
+      }
+      else {
+        fprintf(stderr, "Invalid algorithm %s selected. Use one of a=sub or a=div\n", argv[i]);
+        return 1;
+      }
+    }
     else if ( strncmp(argv[i], "gmin=", 5) == 0 ) {
       if ( sscanf(argv[i] + 5, "%lf", &gmin) != 1 ) {
         fprintf(stderr, "Syntax error on %s\n", argv[i]);
         return 1;
       }
+      gmin_set = true;
     }
     else if ( strncmp(argv[i], "gmax=", 5) == 0 ) {
       if ( sscanf(argv[i] + 5, "%lf", &gmax) != 1 ) {
         fprintf(stderr, "Syntax error on %s\n", argv[i]);
         return 1;
       }
+      gmax_set = true;
+    }
+    else if ( strncmp(argv[i], "gamma=", 6) == 0 ) {
+      if ( sscanf(argv[i] + 6, "%lf", &gamma) != 1 ) {
+        fprintf(stderr, "Syntax error on %s\n", argv[i]);
+        return 1;
+      }
+    }
+    else if ( strncmp(argv[i], "alpha=", 6) == 0 ) {
+      if ( sscanf(argv[i] + 6, "%lf", &alpha) != 1 ) {
+        fprintf(stderr, "Syntax error on %s\n", argv[i]);
+        return 1;
+      }
     }
     else if ( strncmp(argv[i], "b=", 2) == 0 ) {
-      if ( sscanf(argv[i] + 2, "%d", &global_field_blur_size) != 1 ) {
+      if ( sscanf(argv[i] + 2, "%d", &flat_field_blur_size) != 1 ) {
+        fprintf(stderr, "Syntax error on %s\n", argv[i]);
+        return 1;
+      }
+    }
+    else if ( strncmp(argv[i], "f=", 2) == 0 ) {
+      if ( sscanf(argv[i] + 2, "%d", &local_field_blur_size) != 1 ) {
         fprintf(stderr, "Syntax error on %s\n", argv[i]);
         return 1;
       }
@@ -311,6 +411,30 @@ int main(int argc, char *argv[])
     }
   }
 
+
+  if ( !gmin_set ) {
+    switch ( algo ) {
+    case algo_sub :
+      gmin = gmin_sub;
+      break;
+    case algo_div :
+      gmin = gmin_div;
+      break;
+    }
+  }
+
+  if ( !gmax_set ) {
+    switch ( algo ) {
+    case algo_sub :
+      gmax = gmax_sub;
+      break;
+    case algo_div :
+      gmax = gmax_div;
+      break;
+    }
+  }
+
+
   if ( !input_directory_name ) {
     input_directory_name = ".";
   }
@@ -326,58 +450,70 @@ int main(int argc, char *argv[])
   }
 
 
-  if ( input_files.size() < 2 ) {
-    fprintf(stderr, "at least 2 input images need to create a differential movie, only %zu found\n", input_files.size());
-    return 1;
-  }
-
   sort(input_files.begin(), input_files.end(), less<string>());
 
 
-  i = 0;
-  while ( i < input_files.size() && !load_image(prev, input_files[i], &first_pwr) ) {
-    ++i;
-  }
+  for ( j = 0, i = 0; i < input_files.size(); ++i ) {
 
-  if ( i == input_files.size() ) {
-    fprintf(stderr, "fatal: at least 2 input images need to create a differential movie\n");
-    return 1;
-  }
+    double mv = 0, pw = 0;
 
-  // fisrt = prev.clone();
-
-  for ( j = 0, ++i; i < input_files.size(); ++i ) {
-
-    while ( i < input_files.size() && !load_image(current, input_files[i],  &current_pwr) ) {
-      ++i;
+    if ( !load_image(current, input_files[i]) ) {
+      continue;
     }
 
-    if ( current.data ) {
+    get_energy_metrics(current, &current_mean, &current_pwr);
+    //printf("%s: mean=%g pwr=%g\n", input_files[i].c_str(), current_mean, current_pwr);
 
-      diff = current * (first_pwr / current_pwr) - prev;
-      //diff = current - prev;
+    current -= current_mean;
+    current /= current_pwr;
+
+    if ( !prev.data ) {
+      // this is fisrt image in sequence
       prev = current;
-
-      diff -= mean(diff);
-
-      if ( dump_histogram ) {
-        dump_hist(diff);
-      }
-
-      threshold_pixels(diff, gmin, gmax);
-
-      sprintf(outfilename, "%s/frame%03zu.tif", output_directory_name, j);
-
-      normalize(diff, diff, 0, 255, NORM_MINMAX);
-      diff.convertTo(diff, CV_8UC1);
-
-      if ( !imwrite(outfilename, diff) ) {
-        fprintf(stderr, "fatal: imwrite(%s) fails\n",outfilename);
-        return 1;
-      }
-
-      ++j;
+      continue;
     }
+
+    // next image in sequence
+
+    get_energy_metrics(current, &mv, &pw);
+    printf("%s: current_mean=%g -> mv=%g\n", input_files[i].c_str(), current_mean, mv);
+
+    if ( algo == algo_sub ) {
+      diff = current * (1 + alpha) - prev;
+    }
+    else {
+      // fixme: div not implemented yet
+      diff = current * (1 + alpha) - prev;
+    }
+
+    prev = current;
+
+
+    threshold_pixels(diff, gmin, gmax);
+
+    if ( dump_histogram ) {
+      sprintf(outfilename, "%s/frame%03zu.hist", output_directory_name, j);
+      dump_hist(outfilename, diff, gmin, gmax);
+    }
+
+    if ( gamma == 1 ) {
+      normalize_pixels(diff, gmin, gmax, 0, 255);
+    }
+    else {
+      normalize_pixels(diff, gmin, gmax, 0, 1);
+      pow(diff, gamma, diff);
+      normalize_pixels(diff, 0, 1, 0, 255);
+    }
+
+    diff.convertTo(diff, CV_8UC1);
+
+    sprintf(outfilename, "%s/frame%03zu.tif", output_directory_name, j);
+    if ( !imwrite(outfilename, diff) ) {
+      fprintf(stderr, "fatal: imwrite(%s) fails\n", outfilename);
+      return 1;
+    }
+
+    ++j;
 
   }
 
